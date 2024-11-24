14d8e0d9afcf2cf2c54e51b4db8a016f1016ca67
Rohan Maiti <maitirohan2019@gmail.com>
2024-11-24 23:57:18 +0530

    change in card comp

--- frontend/src/component/card.js
+++ frontend/src/component/card.js
@@ -7,29 +7,29 @@ import Button from '@mui/material/Button';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
 
-export default function MultiActionAreaCard() {
+export default function MultiActionAreaCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          image=""
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {props.title ?props.title : "Title here"}
           </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {/* <Typography variant="body2" sx={{ color: 'text.secondary' }}>
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
-          </Typography>
+          </Typography> */}
         </CardContent>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
+          Click
         </Button>
       </CardActions>
     </Card>
