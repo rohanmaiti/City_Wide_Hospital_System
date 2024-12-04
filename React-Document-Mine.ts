3b9faa30a729b0c9be4b154c2a47ba12e02e27a7
Rohan Maiti <132077931+rohanmaiti@users.noreply.github.com>
2024-12-04 23:58:01 +0530

    Update React-Document-Mine.ts

--- React-Document-Mine.ts
+++ React-Document-Mine.ts
@@ -379,7 +379,5 @@ fileInputRef.current.value = "" clears the file input field without breaking the
 * code -r project // command  of vs code terminal to open some directory 
 * Learned one interseting thing ... why when we write useEffect everything runs two times initially ?
 * >> as in index.js page there is the App component is under   <React.StrictMode> tag which run the code 2 time 1st time for checking and 2nd time for actual rendering 
-* 
-*   
-        
+*     
  */
